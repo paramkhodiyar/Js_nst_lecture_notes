86312b29f6e7c7b2d7941c54401d0069bfad2e3d
Param Khodiyar <paramkhodiya1008@gmail.com>
2025-01-15 11:43:19 +0530

    Lecture over, basics of array and object done and noted

--- LEC_on_ifelse/index.js
+++ LEC_on_ifelse/index.js
@@ -58,3 +58,58 @@ switch(day){
     default:
         console.log("Invalid Day");
 }
+
+// Ternary Operator is used to write if else in a single line. It is also called conditional operator.
+// Syntax: condition ? expression1 : expression2
+// If the condition is true then expression1 will be executed otherwise expression2 will be executed.
+
+const age = 18;
+let isholdinglicense = true;
+const iseligibletodrive = (age >= 18 && isholdinglicense) ? "You can drive" : "You are not an adult yet!";
+console.log(iseligibletodrive); // Output: You can drive
+
+
+// Loops in javascript 
+// For loop and while loop 
+// Do while loop is also there but it is not used much. It is similar to while loop but the difference is that it will execute the code block at least once even if the condition is false.
+// Syntex of for loop is for(initialization; condition; increment/decrement){}
+// Syntex of while loop is while(condition){}
+
+for (const i=0; i<5; i++){
+    console.log(i);
+} // Code wont run as i is constant and we cannot change the value of constant variable.
+
+for (const i=0; i<5; i++){
+    console.log("check",i);
+    if (i===0){
+        break;
+    }
+} // Code will run only once as we have used break statement. Wont give any error.
+for (let i=0; i<5; i++){
+    console.log(i);
+} // Output: 0 1 2 3 4
+
+for (let i=0;i++,i<5;){
+    console.log(i);
+} // Output: 1 2 3 4 5
+
+// Note the difference between the above two codes, in the first code the value of i is printed first and then incremented but in the second code the value of i is incremented first and then printed.
+
+// Arrays and objects in js
+// Array is a collection of elements of same or different data types. It is a type of object. Similar to what we call list in python.
+const arr = [1,2,3,4,5];
+console.log(arr); // Output: [1,2,3,4,5]
+console.log(arr[0]); // Output: 1
+console.log(arr[5]); // Output: undefined, note its not an error, thats the speciality of js.
+console.log(arr.length); // Output: 5
+
+// Objects are similar to dictionaries in python. It is a collection of key value pairs. It is also a type of object.
+const obj = {
+    name: "John",
+    age: 30,
+    isAdult: true
+}
+console.log(obj); // Output: {name: "John", age: 30, isAdult: true}
+console.log(obj.name); // Output: John
+console.log(obj.age); // Output: 30
+console.log(obj.isAdult); // Output: true
